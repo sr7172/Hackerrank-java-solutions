af339919e4c922c90024554a691b544c87ed932a
sr7172 <55453722+sr7172@users.noreply.github.com>
2019-09-17 10:58:43 -0500

    Add files via upload

---
+++ InputOutput.java
@@ -0,0 +1,18 @@
+package edu.truman.cs.practice.shreeya.welcome;
+
+import java.util.Scanner;
+
+public class InputOutput {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
---
+++ Welcome.java
@@ -0,0 +1,10 @@
+package edu.truman.cs.practice.shreeya.welcome;
+
+public class Welcome {
+	
+	public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.print("Hello, Java.");
+    }
+
+}
